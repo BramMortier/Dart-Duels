36f72e93e5f9b9adf783d5d4890f3c7381f3c813
BramMortier <79015250+BramMortier@users.noreply.github.com>
2023-06-22 01:13:30 +0200

    titlescreen added

--- src/pages/Titlescreen/Titlescreen.jsx
+++ src/pages/Titlescreen/Titlescreen.jsx
@@ -1,7 +1,22 @@
 import "./titlescreen.scss";
 
 const Titlescreen = () => {
-    return <div>Titlescreen</div>;
+    return (
+        <main className="title-screen">
+            <div className="title-screen__container">
+                <div className="title-screen__col">
+                    <img src="/assets/logo.png" alt="dart duels logo" />
+                    <button className="title-screen__start-btn">
+                        Press any button to start
+                        <span className="title-screen__start-btn-line"></span>
+                    </button>
+                </div>
+                <div className="title-screen__col">
+                    <img src="/assets/titlescreen.png" alt="titlescreen" />
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Titlescreen;
